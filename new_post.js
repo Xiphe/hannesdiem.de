90f693348c7dfd7e01fd341f40459cfe0b996f34
DiemBuildBot <diembuildbot@xiphe.net>
2016-04-28 23:13:03 +0000

    Updating to Xiphe/diem-musik.de@f8e732dc7868dd7450e34fedeecb2094c22520ef.

--- new_post.js
+++ new_post.js
@@ -3,7 +3,15 @@
 const fs = require('fs');
 const path = require('path');
 const date = new Date();
-const topic = process.argv[2];
+const minimist = require('minimist');
+const argv = minimist(process.argv, {
+  alias: {
+    t: 'tagesform'
+  }
+});
+const isTagesform = !!argv.tagesform;
+let topic = process.argv[2];
+let other = '';
 
 function leftPad(str, length, pad) {
   str = str + '';
@@ -13,20 +21,36 @@ function leftPad(str, length, pad) {
   return str;
 }
 
+function toSlug(str) {
+  return str.toLowerCase().replace(/[^a-z0-9]/gmi, '-');
+}
+
 const dateStr = `${date.getFullYear()}-`
  + `${leftPad(date.getMonth() + 1, 2, 0)}-`
  + leftPad(date.getDate(), 2, 0);
 const timeStr = `${leftPad(date.getHours(), 2, 0)}:`
   + `${leftPad(date.getMinutes(), 2, 0)}:`
   + leftPad(date.getSeconds(), 2, 0);
-const fileName = path.join(__dirname, '_posts', `${dateStr}-${topic}.md`);
+
+const cover = isTagesform ? 'tagesform_cover_jtbc9y' : 'diem_himmel_creltn'
+const tags = isTagesform ? 'podcasts tagesform ' : '';
+const layout = isTagesform ? 'tagesform' : 'post';
+const folder = isTagesform ? '_posts/tagesform' : '_posts';
+
+if (isTagesform) {
+  topic = `Tagesform ${argv.tagesform}`;
+  other += `\nepisode: ${argv.tagesform}`;
+}
+
+const fileName = path.join(__dirname, folder, `${dateStr}-${toSlug(topic)}.md`);
+
 
 const contents = `---
-layout: post
-cover: 'assets/images/cover/small_path.jpg'
+layout: ${layout}
+cover: ${cover}${other}
 title: ${topic}
 date: ${dateStr} ${timeStr}
-tags:
+tags: ${tags}
 navigation: true
 logo: 'assets/images/logo.svg'
 ---
