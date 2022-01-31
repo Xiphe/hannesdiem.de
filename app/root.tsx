ce8afb5592315648fcb66ee0dfc2d91b44469958
Xiphe <github@xiphe.net>
2022-01-31 20:44:58 +0100

    chore: install tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
@@ -7,9 +8,14 @@ import {
   ScrollRestoration,
 } from 'remix';
 import type { MetaFunction } from 'remix';
+import styles from './tailwind.css';
+
+export let links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 export const meta: MetaFunction = () => {
-  return { title: 'New Remix App' };
+  return { title: 'Hannes Diem on Remix' };
 };
 
 export default function App() {
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx}'],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
