22ac94d8b4b61661cea62802f46673119ba254a2
Xiphe <github@xiphe.net>
2022-02-01 21:36:54 +0100

    fix: use xx notation for feed date timezone

--- app/util/tagesform/formatDate.ts
+++ app/util/tagesform/formatDate.ts
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
 
 export default function formatTagesformFeedDate(date: Date | number) {
-  return format(date, 'iii, dd MMM yyyy HH:mm:ss OOO');
+  return format(date, 'iii, dd MMM yyyy HH:mm:ss xx');
 }
