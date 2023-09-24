3156456c47afa53207f305edd9b0b9719efc74ff
Xiphe <github@xiphe.net>
2023-09-25 01:45:10 +0200

    fix: responsive Letter

--- components/Letter.tsx
+++ components/Letter.tsx
@@ -28,19 +28,19 @@ export function Letter({
   return (
     <div
       className={clsx(
-        "prose lg:prose-xl dark:prose-invert shadow-2xl pl-24 pr-12 pt-16 pb-24",
-        "font-serif mx-auto my-16 bg-orange-50  text-blue-800 dark:bg-gray-950 dark:text-yellow-50"
+        "prose lg:prose-xl dark:prose-invert shadow-2xl pl-8 pr-4 sm:pl-24 sm:pr-12 pt-16 pb-24",
+        "font-serif mx-auto sm:my-16 bg-orange-50  text-blue-800 dark:bg-gray-950 dark:text-yellow-50"
       )}
     >
-      <TitleComponent className="!my-2 text-center pr-12">
+      <TitleComponent className="!my-2 text-center pr-4 sm:pr-12">
         {title}
       </TitleComponent>
       {subtitle ? (
-        <SubTitleComponent className="!mt-2 !mb-4 italic font-light text-center pr-12">
+        <SubTitleComponent className="!mt-2 !mb-4 italic font-light text-center pr-4 sm:pr-12">
           {subtitle}
         </SubTitleComponent>
       ) : null}
-      <p className="font-fuggles text-4xl text-center pr-12 !mt-2 mb-8">
+      <p className="font-fuggles text-4xl text-center pr-4 sm:pr-12 !mt-2 mb-8">
         {authors?.map(({ name, description, link }, i) => (
           <>
             {link ? (
